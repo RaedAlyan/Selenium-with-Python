8069d52e64def31fc842c4e2a3310ec595d50028
Raed Eleyan <raedmalyan@gmail.com>
2024-07-02 17:28:05 +0300

    automated a login scenario

---
+++ automation_login.py
@@ -0,0 +1,25 @@
+"""
+    We want to automate the following steps:
+        1- Open a web browser (Chrome).
+        2- Open a URL: https://www.saucedemo.com/
+        3- Provide an email: standard_user.
+        4- Provide a password: secret_sauce.
+        5- Click on the login button.
+        6- Verify the title.
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service_obj = Service("D:\\Files\\Selenium\\Drivers\\chromedriver\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+driver.refresh()
+driver.find_element(By.NAME, 'user-name').send_keys('standard_user')
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+print(driver.title)
+driver.close()
+driver.quit()
